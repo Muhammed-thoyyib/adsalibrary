cb6837b5c572d79a466c73e39b7b2ef8d23140cf
Fazulu Fazulurahman <ffazulurahman@gmail.com>
2026-05-05 11:29:47 +0000

    apikey

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5579487584-cabc8",
   "appId": "1:1035803789220:web:9fb2f196a5bc07ebeaa331",
-  "apiKey": "AIzaSyCnOmRtkYnnz2Y3EsYFyIlxUNQLER3e-Pw",
+  "apiKey": "AIzaSyCnOmRtkYnnn2Y3EsYFyIlxUNQLER3e-Pw",
   "authDomain": "studio-5579487584-cabc8.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1035803789220"
